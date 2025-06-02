eb144e0c0ce4e9fcefacb22018eb0b374db94415
Knomo <knomolestes@gmail.com>
2025-06-01 21:46:20 -0500

    added start and finish times to runner

--- gnubg-script-runner.sh
+++ gnubg-script-runner.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Print initial time
+initial_time=$(date +"%Y-%m-%d %H:%M:%S %Z")
+echo "Script started at: $initial_time"
+
 ./gnubg -t -c gnubg_commands.cmd
+
+# Print finish time
+finish_time=$(date +"%Y-%m-%d %H:%M:%S %Z")
+
+echo "Script started at: $initial_time"
+echo "Script finished at: $finish_time"
